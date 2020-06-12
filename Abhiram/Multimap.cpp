5430b2bc847c2a69732e1972f30a47c1e4662538
abhiram2600 <abhiram2600@users.noreply.github.com>
2020-06-12 12:01:12 +0530

    map and multimap

---
+++ Abhiram/Multimap.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define sp "\t"
+#define f(n) for (int i = 0; i < int(n); i++)
+#define ll long long int
+#define ln long int
+#define pb push_back
+#define mk make_pair
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <map>
+using namespace std;
+
+void display(multimap<int,int> mp)
+{
+	cout<<"displaying"<<endl;
+	for(auto it = mp.begin();it!= mp.end();it++)
+		cout<<it->first<<sp<<it->second<<endl;
+}
+
+
+int main()
+{
+	multimap<int,int> mp;
+	for(int i=1;i<=10;i++)
+		mp.insert(mk(i,i+10));
+	display(mp);
+	mp.insert(mk(2,32));//this will be inserted into map
+	display(mp);
+
+	mp.erase(mp.begin(),mp.find(3));
+	cout<<"removing elements less than 3"<<endl;
+
+	display(mp);
+	cout<<"lower bound"<<sp<<mp.lower_bound(5)->first<<sp<<mp.lower_bound(5)->second<<endl;
+	cout<<"upper bound"<<sp<<mp.upper_bound(5)->first<<sp<<mp.upper_bound(5)->second<<endl;
+
+
+
+}
